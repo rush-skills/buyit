cc8a8f4a0837402aaa109b41d54bd4a12939d41c
Ankur Singh <ankur13019@iiitd.ac.in>
2015-07-26 02:34:05 +0530

    basic classifier complete

--- scraper.rb
+++ scraper.rb
@@ -66,14 +66,15 @@ get '/*' do
   price = price.gsub(',','').to_i
   value = (price - average)/average.to_f
 
-  data_points =  {:sentiment => sentiment, :rating => rating.to_f,:rating_count => rating_count.to_i,:reviews_count => reviews_count.to_i,
+  data_points =  {:sentiment => sentiment.to_i, :rating => rating.to_f,:rating_count => rating_count.to_i,:reviews_count => reviews_count.to_i,
     :seller_review => seller_review.to_f, :other_sellers => other_sellers.to_i, :title => title, :photo_count => photo_count,
     :content_length => content_length, :more_styles => more_styles, :questions => questions.to_i, :answers => answers.to_i,
     :discount => discount.to_i, :price => price, :high => high, :low => low, :average => average, value: value}
 
   result = algo(data_points)
-  puts result
-  return data_points.to_json
+  # puts result
+  # return data_points.to_json
+  return result.to_s
 end
 
 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
@@ -81,7 +82,7 @@ r1, r2, r3, r4, r5, r6, r7, r8, r9, r10 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 def algo(data)
   r1 = compute_rating(data)
   r2 = compute_seller(data)
-  r3 = compute_other_sellers(data)
+  r3 = compute_other_seller(data)
   r4 = compute_photo data
   r5 = compute_content data
   r6 = compute_more_styles data
@@ -90,6 +91,9 @@ def algo(data)
   r9 = compute_price_diff data
   r10 = compute_sentiment data
 
+  total = 35 * r1 + 10 * r2 + 5 * r3 + 5 * r4 + 10 * r5 + 5 * r6 + 10 * r7 + 5 * r8 + 10 * r9 + 5 * r10
+  final_rating = total/100.0
+  final_rating.round
 
 end
 
@@ -134,4 +138,101 @@ def compute_seller(data)
   r2
 end
 
+def compute_other_seller(data)
+  if data[:other_sellers] == 0
+    r3 = 0
+  elsif data[:other_sellers] < 3
+    r3 = 1
+  else
+    r3 = 2
+  end
+  r3
+end
 
+def compute_photo(data)
+  if data[:photo_count] == 0
+    r4 = 0
+  elsif data[:photo_count] == 1
+    r4 = 1
+  else
+    r4 = 2
+  end
+  r4
+end
+
+def compute_content(data)
+  unless data[:content_length]
+    0
+  end
+  if data[:content_length] < 1500
+    r5 = 0
+  elsif data[:content_length] > 1500 and data[:content_length] < 5000
+    r5 = 1
+  else
+    r5 = 2
+  end
+  r5
+end
+
+def compute_more_styles(data)
+  if data[:more_styles] == 0
+    r6 = 0
+  elsif data[:more_styles] == 1
+    r6 = 1
+  else
+    r6 = 2
+  end
+  r6
+end
+
+def compute_qa(data)
+  unless data[:questions] and data[:answers]
+    0
+  end
+  ratio = data[:answers]/data[:questions].to_f
+  puts "Ratio = " + ratio.to_s
+  if ratio < 0.25 or ratio.nan?
+    r7 = 0
+  elsif ratio > 0.25 and ratio < 0.5
+    r7 = 1
+  else
+    r7 = 2
+  end
+  r7
+end
+
+def compute_discount(data)
+  unless data[:discount]
+    2
+  end
+  if data[:discount] > 75
+    r8 = 0
+  elsif data[:discount] >= 35
+    r8 = 1
+  else
+    r8 = 2
+  end
+  r8
+end
+
+def compute_price_diff(data)
+  if data[:value].abs > 1
+    r9 = 0
+  elsif data[:value].abs > 0.25 and data[:value].abs < 1
+    r9 = 1
+  else
+    r9 = 2
+  end
+  r9
+end
+
+def compute_sentiment(data)
+  if data[:sentiment] < 40
+    r10 = 0
+  elsif data[:sentiment] < 60
+    r10 = 1
+  else
+    r10 = 2
+  end
+  r10
+end  
