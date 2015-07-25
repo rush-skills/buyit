d42ec81367c8fc8f976bd557adf77742678be1fb
Ankur Singh <ankur13019@iiitd.ac.in>
2015-07-25 19:20:14 +0530

    simple parsing snapdeal

--- scraper.rb
+++ scraper.rb
@@ -15,17 +15,25 @@ get '/*' do
   # Fetch and parse HTML document
   url = params["splat"][0].to_s
   doc = Nokogiri::HTML(open("http://" + url))
-  rating = doc.css('.sd-product-main-rating')[0]["md-data-rating"]
+
+  rating = doc.css('.sd-product-main-rating')[0]["md-data-rating"] if doc.css('.sd-product-main-rating')[0]
+  rating_count = doc.css('.showRatingTooltip')[0].content.to_s.split(" ")[0] if doc.css('.showRatingTooltip')[0]
+  reviews_count = doc.css('.review_land')[0].content.to_s.split(" ")[0] if doc.css('.review_land')[0]
+  seller_review = doc.css('.pdp-e-seller-info-score .roboto')[0].content.to_s.split(" ")[0] if doc.css('.pdp-e-seller-info-score .roboto')[0]
+  other_sellers = doc.css('.pdp-other-sellers span')[0].content.to_s.split(" ")[0] if doc.css('.pdp-other-sellers span')[0]
+  title = doc.css('.pdp-e-i-head')[0].content.to_s
 
   #Selenium
-  # driver = Selenium::WebDriver.for :firefox
-  # driver.navigate.to "http://topsy.com/s?q=moto%20g"
-  # sentiment = driver.execute_script("return $('.sentiment-score').html()")
-  # driver.quit
+  driver = Selenium::WebDriver.for :firefox
+  str = "http://topsy.com/s?q="+title
+  final_url = URI::encode(str)
+  driver.navigate.to final_url
+  sentiment = driver.execute_script("return $('.sentiment-score').html()")
+  driver.quit
   #end selenium
 
   #hard code to skip selenium
-  sentiment = "1"
+  # sentiment = "1"
 
-  return {:sentiment => sentiment, :rating => rating}.to_json
+  return {:sentiment => sentiment, :rating => rating,:rating_count => rating_count,:reviews_count => reviews_count, :seller_review => seller_review, :other_sellers => other_sellers, :title => title}.to_json
 end
